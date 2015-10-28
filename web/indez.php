c58acb7a55705833954b9f068ba4e27f304a8984
Emanuel Lucena <emanuel.lucena14@hotmail.com>
2015-10-27 23:44:09 -0200

    Tela Cadastro
    Tela Cadastro, ainda não tem acesso as outras, no entanto ao clicar
    nelas, irá redirecionar a página do google, por ainda não estarem
    prontas, o arquivo vai com o nome de indez.php não por erro, mas sim por
    na hora que estava fazendo, não da erro e apagar sem querer rsrs

---
+++ web/indez.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="pt-BR" xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#">
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="www.google.com" />
+	<link rel="icon"  href="www.google.com" />
+	<meta property="og:image" content="http://www.google.com"/>
+	<link rel="image_src" href="http://www.google.com" / >
+	<title>Chrono </title>
+	
+	<!--[if lt IE 9]>
+	<script type="text/javascript" src="http://www.google.com"></script>
+	<![endif]-->
+	
+	<link rel="alternate" type="application/rss+xml" title="Feed Chrono  &raquo;" href="http://www.google.com" />
+<link rel="alternate" type="application/rss+xml" title="Chrono &raquo;  Feed de comentários" href="http://www.google.com" />
+<link rel='stylesheet' id='general-css'  href='http://www.cursoemvideo.com/wp-content/themes/academy/style.css?ver=3.9.2' type='text/css' media='all' />
+<script type='text/javascript' src='http://www.google.com'></script>
+<script type='text/javascript' src='http://www.google.com'></script>
+<script type='text/javascript' src='http://www.google.com'></script>
+<script type='text/javascript' src='http://www.google.com'></script>
+<script type='text/javascript' src='http://www.google.com'></script>
+<script type='text/javascript' src='http://google.com'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var options = {"templateDirectory":"http:\/\/www.google.com\/wp-content\/themes\/academy\/"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://www.google.com.com/wp-content/themes/academy/js/jquery.raty.min.js?ver=3.9.2'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var options = {"templateDirectory":"http:\/\/www.google.com\/wp-content\/themes\/academy\/"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://www.google.com/wp-content/themes/academy/js/general.js?ver=3.9.2'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.google.com" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://www.google.com/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 3.9.2" />
+<link rel="shortcut icon" href="http://google.com/wp-content/uploads/2014/04/logo-curso-video.png" />
+<style type="text/css">.featured-content{
+	}body, input, select, textarea{font-family:Open Sans, Arial, Helvetica, sans-serif;}h1,h2,h3,h4,h5,h6, .header-navigation div > ul > li > a{font-family:Crete Round, Arial, Helvetica, sans-serif;}input[type="submit"], input[type="button"], .button, .jp-play-bar, .jp-volume-bar-value, .free-course .course-price .price-text, .lessons-listing .lesson-attachments a, ul.styled-list.style-4 li:before, .woocommerce a.button.alt, .woocommerce button.button.alt, .woocommerce input.button.alt, .woocommerce #respond input#submit.alt, .woocommerce #content input.button.alt, .woocommerce-page a.button.alt, .woocommerce-page button.button.alt, .woocommerce-page input.button.alt, .woocommerce-page #respond input#submit.alt, .woocommerce-page #content input.button.alt, .woocommerce a.button.alt:hover, .woocommerce button.button.alt:hover, .woocommerce input.button.alt:hover, .woocommerce #respond input#submit.alt:hover, .woocommerce #content input.button.alt:hover, .woocommerce-page a.button.alt:hover, .woocommerce-page button.button.alt:hover, .woocommerce-page input.button.alt:hover, .woocommerce-page #respond input#submit.alt:hover, .woocommerce-page #content input.button.alt:hover{background-color:#007eae;}.free-course .course-price .corner{border-top-color:#007eae;border-right-color:#007eae;}.button.secondary, .quiz-listing .question-number, .lessons-listing .lesson-title .course-status, .course-price .price-text, .course-price .corner, .course-progress span, .questions-listing .question-replies, .course-price .corner-background, .user-links a:hover, .payment-listing .expanded .toggle-title:before, .styled-list.style-5 li:before, .faq-toggle .toggle-title:before, .lesson-toggle, ul.styled-list.style-1 li:before, .woocommerce a.button, .woocommerce button.button, .woocommerce input.button, .woocommerce #respond input#submit, .woocommerce #content input.button, .woocommerce-page a.button, .woocommerce-page button.button, .woocommerce-page input.button, .woocommerce-page #respond input#submit, .woocommerce-page #content input.button, .woocommerce a.button:hover, .woocommerce button.button:hover, .woocommerce input.button:hover, .woocommerce #respond input#submit:hover, .woocommerce #content input.button:hover, .woocommerce-page a.button:hover, .woocommerce-page button.button:hover, .woocommerce-page input.button:hover, .woocommerce-page #respond input#submit:hover, .woocommerce-page #content input.button:hover{background-color:#5ea5d7;}a, a:hover, a:focus, ul.styled-list li > a:hover{color:#5ea5d7;}.button.dark, .jp-gui, .jp-controls a, .jp-video-play-icon, .header-wrap, .header-navigation ul ul, .select-menu, .search-form, .mobile-search-form, .login-button .tooltip-text, .footer-wrap, .site-footer:after, .site-header:after, .widget-title{background-color:#0f1f2f;}.jp-jplayer{border-color:#6d6d6d;}.widget-title{border-bottom-color:#6d6d6d;}::-moz-selection{background-color:#007eae;}::selection{background-color:#007eae;}.lessons-listing .lesson-item.completed .lesson-title a {
+padding-right: 110px;
+display: inline-block;
+line-height: 30px;
+background:  no-repeat center right;
+}
+
+.single-lesson .widget .completed a { }</style><style type="text/css">@font-face {
+						font-family: "Crete Round";
+						src: url("http://www.google.com");
+						src: url("http://www.google.com") format("embedded-opentype"),
+							 url("http://www.google.com") format("woff"),
+							 url("http://www.google.com") format("truetype"),
+							 url("http://www.google.com") format("svg");
+						font-weight: normal;
+						font-style: normal;
+					}</style><script type="text/javascript">
+			WebFontConfig = {google: { families: [ "Open Sans:400,400italic,600" ] } };
+			(function() {
+				var wf = document.createElement("script");
+				wf.src = ("https:" == document.location.protocol ? "https" : "http") + "://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js";
+				wf.type = "text/javascript";
+				wf.async = "true";
+				var s = document.getElementsByTagName("script")[0];
+				s.parentNode.insertBefore(wf, s);
+			})();
+			</script></head>
+<body class="blog">
+	<div class="site-wrap">
+		<div class="header-wrap">
+			<header class="site-header">
+				<div class="row">
+					<div class="site-logo left">
+						<a href="http://www.google.com/" rel="home">
+							 Chrono 
+						</a>
+					</div>
+					<!-- /logo -->
+					<div class="header-options right clearfix">					
+						<div class="login-options right">
+												
+							<div class="button-wrap left tooltip login-button">
+								<a href="#" class="button dark"><span class="button-icon login"></span>Entrar</a>
+								<div class="tooltip-wrap">
+									<div class="tooltip-text">
+										<form action="http://www.google.com" class="ajax-form popup-form" method="POST">
+											<div class="message"></div>
+											<div class="field-wrap">
+												<input type="text" name="user_login" value="Usu&aacute;rio" />
+											</div>
+											<div class="field-wrap">
+												<input type="password" name="user_password" value="Senha" />
+											</div>
+											<div class="button-wrap left nomargin">
+												<a href="#" class="button submit-button">Entrar</a>
+											</div>											
+																						<div class="button-wrap switch-button left">
+												<a href="#" class="button dark" title="Perdi minha senha">
+													<span class="button-icon help"></span>
+												</a>
+											</div>
+											<input type="hidden" name="user_action" value="login_user" />
+											<input type="hidden" name="user_redirect" value="" />
+											<input type="hidden" name="nonce" class="nonce" value="c8b5aff44b" />
+											<input type="hidden" name="action" class="action" value="themex_update_user" />
+										</form>
+									</div>
+								</div>
+								<div class="tooltip-wrap password-form">
+									<div class="tooltip-text">
+										<form action="http://www.google.com" class="ajax-form popup-form" method="POST">
+											<div class="message"></div>
+											<div class="field-wrap">
+												<input type="text" name="user_email" value="Email" />
+											</div>
+											<div class="button-wrap left nomargin">
+												<a href="#" class="button submit-button">Redefinir Senha</a>
+											</div>
+											<input type="hidden" name="user_action" value="reset_password" />
+											<input type="hidden" name="nonce" class="nonce" value="c8b5aff44b" />
+											<input type="hidden" name="action" class="action" value="themex_update_user" />
+										</form>
+									</div>
+								</div>
+							</div>
+														<div class="button-wrap left">
+								<a href="http://www.google.com" class="button">
+									<span class="button-icon register"></span>Cadastrar								</a>
+							</div>
+																			</div>
+						<!-- /login options -->										
+						<div class="search-form right">
+							<form role="search" method="GET" action="http://www.google.com">
+	<input type="text" value="" name="s" />
+</form>						</div>
+						<!-- /search form -->
+											</div>
+					<!-- /header options -->
+					<div class="mobile-search-form">
+						<form role="search" method="GET" action="http://www.google.com/">
+	<input type="text" value="" name="s" />
+</form>					</div>
+					<!-- /mobile search form -->
+					<nav class="header-navigation right">
+						<div class="menu"><ul id="menu-main-menu" class="menu">
+						<li id="menu-item-2137" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2137">
+						<a href="http://www.google.com">Home</a></li>
+<li id="menu-item-2143" class="menu-item menu-item-type-taxonomy menu-item-object-course_category menu-item-has-children menu-item-2143">
+<a href="http://www.google.com">Agenda</a>
+<ul class="sub-menu">
+	<li id="menu-item-24624" class="menu-item menu-item-type-post_type menu-item-object-course menu-item-24624">
+	<a href="http://www.google.com">Cadastrar Nova Agenda</a></li>
+</ul>
+</li>
+<li id="menu-item-2135" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2135">
+
+
+</ul></div>						
+						<div class="select-menu">
+							<span></span>
+							<select><option value="http://www.google.com/">Home</option>
+							<option value="http://www.google.com">Cadastrar Agenda</option>
+							
+							<option value="http://www.google.com">Chrono</option></select>							
+						</div>
+						<!--/ select menu-->
+					</nav>
+					<!-- /navigation -->						
+				</div>			
+			</header>
+			<!-- /header -->
+		</div>
+		<div class="featured-content">
+			<div class="substrate">
+				<img src="http://www.google.com" class="fullwidth" alt="" />			</div>
+						<div class="row">
+							<div></div>
+				<!-- /page title -->				
+						</div>
+				
+		</div>
+		<!-- /featured -->
+		<div class="main-content">
+			<div class="row"><div class="eightcol column">
+	<h1>Cadastrar</h1>
+	<form class="ajax-form formatted-form" action="http://www.google.com" method="POST">
+		<div class="message"></div>
+		<div class="sixcol column">
+			<div class="field-wrapper">
+				<input type="text" name="user_login" placeholder="Usu&aacute;rio (sem espa&ccedil;os ou acentos)" />
+			</div>								
+		</div>
+		<div class="sixcol column last">
+			<div class="field-wrapper">
+				<input type="text" name="user_email" placeholder="E-mail (conta v&aacute;lida)" />
+			</div>
+		</div>
+		<div class="clear"></div>
+		<div class="sixcol column">
+			<div class="field-wrapper">
+				<input type="password" name="user_password" placeholder="Senha (pelo menos 4 letras)" />
+			</div>
+		</div>
+		<div class="sixcol column last">
+			<div class="field-wrapper">
+				<input type="password" name="user_password_repeat" placeholder="Repetir Senha" />
+			</div>
+		</div>
+		<div class="clear"></div>			
+				<a href="#" class="button submit-button left"><span class="button-icon register"></span>Cadastrar</a>
+		<div class="form-loader"></div>
+		<input type="hidden" name="user_action" value="register_user" />
+		<input type="hidden" name="user_redirect" value="" />
+		<input type="hidden" name="nonce" class="nonce" value="c8b5aff44b" />
+		<input type="hidden" name="action" class="action" value="themex_update_user" />
+	</form>
+	<div style="display:block;clear:both;background-color: rgba(0,0,0,0.1);width:0px;padding:0px;">
+	</div>
+</div>
+<div class="fourcol column last">
+		<h1>Entrar</h1>
+		<form class="ajax-form formatted-form" action="http://www.google.com" method="POST">
+		<div class="message"></div>
+		<div class="field-wrapper">
+			<input type="text" name="user_login" placeholder="Usu&aacute;rio" />
+		</div>
+		<div class="field-wrapper">
+			<input type="password" name="user_password" placeholder="Senha" />
+		</div>			
+		<a href="#" class="button submit-button left"><span class="button-icon login"></span>Entrar</a>
+				<div class="form-loader"></div>
+		<input type="hidden" name="user_action" value="login_user" />
+		<input type="hidden" name="user_redirect" value="" />
+		<input type="hidden" name="nonce" class="nonce" value="c8b5aff44b" />
+		<input type="hidden" name="action" class="action" value="themex_update_user" />
+	</form>			
+</div>
+<div class="clear"></div>
+								</div>
+			</div>
+			<!-- /content -->
+			<div class="footer-wrap">
+				<footer class="site-footer">
+					<div class="row">
+						<div class="copyright left">
+							<style="vertical-align:middle;width:25px;padding:8px;"/>Chrono &copy; 2015						</div>
+						<nav class="footer-navigation right">
+							<div class="menu-footer-menu-container"><ul id="menu-footer-menu" class="menu">
+							
+<li id="menu-item-2140" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2140">
+<a href="http://www.google.com">Notas de Estudo</a></li>
+</div>						</nav>
+						<!-- /navigation -->				
+					</div>			
+				</footer>				
+			</div>
+			<!-- /footer -->			
+		</div>
+		<!-- /site wrap -->
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42671530-1', 'auto');
+  ga('send', 'pageview');
+
+</script><script type='text/javascript' src='http://www.google.com></script>
+	
+</body>
+</html>
