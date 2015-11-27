f8b171bd01cb7b77e2a9ab570972e79cd350f283
Jonathan Souza <jonathan.ralison@gmail.com>
2015-11-27 02:49:29 -0300

    Novo layout

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -2,17 +2,57 @@
 /* @var $this XController */
 $this->clientScript
         ->registerPackage('normalize')
-        ->registerCssFile('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css');
+        ->registerCssFile('https://fonts.googleapis.com/css?family=Comfortaa:300,700|Maven+Pro:400,500')
+        ->registerCssFile('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css')
+        ->registerCssFile('https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css')
+        ->registerCssFile(Yii::app()->baseUrl . '/build/css/chrono.css')
+        ->registerCssFile(Yii::app()->baseUrl . '/build/css/chrono.css')
+        ->registerScriptFile(Yii::app()->baseUrl . '/build/js/chrono.js');
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="pt">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=content-width">
+        <meta name="viewport" content="width=content-width,initial-scale=1.0">
         <title><?= Yii::app()->name ?></title>
     </head>
     <body>
-        <?= $content ?>
+
+        <aside class="sidebar">
+            <header>
+                Chrono
+                <a href="#" class="btn btn-link" role="button"
+                   aria-label="Menu"><i class="fa fa-bars"></i><span class="sr-only">Menu</span></a>
+            </header>
+            <nav role="navigation">
+                <a href="pages/login.html" data-page="login">Login</a>
+                <a href="pages/register.html" data-page="register">Cadastro de usuário</a>
+                <a href="pages/program.html" data-page="materias">Conteúdo programático</a>
+                <a href="pages/table.html" data-page="table">Tabela</a>
+                <a href="pages/typo.html" data-page="typo">Tipografia</a>
+                <a href="pages/elements.html" data-page="elements">Outros Elementos</a>
+            </nav>
+        </aside>
+
+        <main role="main">
+
+            <?php if ($this->user->hasFlash('success')) : ?>
+                <div class="alert alert-success">
+                    <?= $this->user->getFlash('success') ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($this->user->hasFlash('error')) : ?>
+                <div class="alert alert-danger">
+                    <?= $this->user->getFlash('error') ?>
+                </div>
+            <?php endif; ?>
+
+            <?= $content ?>
+
+        </main>
+
+        <!-- Analytics -->
         <?= $this->renderPartial('/layouts/incl/piwik', null, true) ?>
     </body>
 </html>
